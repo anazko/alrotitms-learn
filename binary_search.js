c3077b1686ecc0160123b467d90f1b9732649928
Andrey Nazarenko <a-naz@mail.ru>
2022-02-11 20:22:00 +0600

    refactor binary-search

--- binary_search.js
+++ binary_search.js
@@ -3,45 +3,34 @@
 
 
 
-var searchInsert = function(nums, target) {
+var search = function(nums, target) {
  
   let start = 0
   let end = nums.length - 1
-  let current = Math.floor((start + end) / 2)
+  let current
 
-  while (nums[current] !== target) {
+  while (start <= end) {
+    current = Math.round( ((end - start) / 2) + start )
     
-    if (nums[current] > target) {
-      end = current
-      current = Math.floor((start + end) / 2)
-    }
-
-    if (nums[current] < target) {
-      start = current
-      current = Math.floor((start + end) / 2)
-    }
-
-    console.log("Start" , start)
-    console.log("End" , end)
-    console.log("Current" , current)
-
     if (nums[current] === target) return current
 
-    if (end - start <= 1) {
-      if (target < nums[0]) return 0
-      if (target > nums[nums.length - 1]) return nums.length
-      return end
+    if (nums[current] > target ) {
+      end = current - 1
+    }
+    else {
+      start = current + 1
     }
 
   }
 
-  return current
+  return -1
 }
 
-// const nums = [2,3,5,6,8,9,10,11]
+
+const nums = [2,3,5,6,8,9,10,11]
 // const nums = [1,3,5,6]
-const nums = [1,3,5]
+// const nums = [1,3,5]
 
 // console.log(searchInsert(nums, 1))
 // console.log(searchInsert(nums, 2))
-console.log(searchInsert(nums, 1))
+console.log(search(nums, 6))
